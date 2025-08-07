46f36faa8131ec52cece2da8ef2519dd3f6060f9
CNTRS <m.contreras.calzon@gmail.com>
2025-08-08 00:16:56 +0200

    Add logic for cell operations

--- src/core/cell.ts
+++ src/core/cell.ts
@@ -1,8 +1,29 @@
 export enum CellStatus {
-	Dead,
-	Alive,
+    Dead = 0,
+    Alive = 1,
 }
 
 export class Cell {
-    // TODO: Implement kata
+    private constructor(private readonly status: CellStatus) {}
+    static create(status: CellStatus): Cell {
+        if (status == null) {
+            throw new Error("Cell status cannot be undefined or null")
+        }
+        return new Cell(status)
+    }
+    isAlive(): boolean {
+        return this.status === CellStatus.Alive
+    }
+    private newStatus(aliveNeighbors: number): CellStatus {
+        if (this.isAlive()) {
+            return aliveNeighbors === 2 || aliveNeighbors === 3 ? CellStatus.Alive : CellStatus.Dead
+        }
+        return aliveNeighbors === 3 ? CellStatus.Alive : CellStatus.Dead
+    }
+    regenerate(neighbors: number): Cell {
+        if (neighbors == null) {
+            throw new Error("Number of neighbors cannot be undefined or null")
+        }
+        return Cell.create(this.newStatus(neighbors))
+    }
 }
